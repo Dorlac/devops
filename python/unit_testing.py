c8da1a81a9c1b08a49496a7e745b64ea268ad9ff
Dorlac <jacobdorlac@gmail.com>
2021-08-23 19:20:22 -0500

    Unit testing

---
+++ python/unit_testing.py
@@ -0,0 +1,3 @@
+# Here is a good reference on setting up a testing framework on vscode: https://code.visualstudio.com/docs/python/testing
+# Note that dependencies will need to be installed. Install dependencies from requirements.txt.
+# Here is the tfplenum requirements: https://gitlab.sil.lab/tfplenum/tfplenum/-/blob/devel/requirements.txt
